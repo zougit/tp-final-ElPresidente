9166962a060c5a48d5e498dcc853c0319ccc52fe
vettese giovanni <vettesegiovanni@gmail.com>
2021-03-07 15:21:12 +0100

    first commit

---
+++ src/main/java/Factions.java
@@ -0,0 +1,10 @@
+public class Factions {
+    int [] capitalist = {5,25};
+    int [] communist = {5,25};
+    int [] liberals = {5,25};
+    int [] religious = {5,25};
+    int [] militarist = {5,25};
+    int [] ecologist = {5,25};
+    int [] nationalist = {5,25};
+    int [] loyalist = {5,25};
+}
